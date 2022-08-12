5ae967542b2fda9c0caa7a82dbf932ca304938dd
masudfcs1 <masudfcs1@gmail.com>
2022-08-12 08:40:28 +0600

    First

--- PHP Basic/superglobals/session.php
+++ PHP Basic/superglobals/session.php
@@ -14,8 +14,12 @@ session_start();
     echo "Session variables are set.<br><br><br>";
     echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
     echo "Favorite animal is " . $_SESSION["favanimal"] . ".<br><br>";
+
+    // session unset
     session_unset();
+
     print_r($_SESSION);
+    // session destroy
     session_destroy();
 
 
